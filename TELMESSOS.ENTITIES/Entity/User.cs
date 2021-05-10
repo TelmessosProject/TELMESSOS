c1a9db2afa9a783cb455ff5fdfd96d978c76a568
Ramazan Karagoz <ramazankaragoz@outlook.com>
2021-05-10 10:25:31 +0300

    Entities Lib. Added

---
+++ TELMESSOS.ENTITIES/Entity/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using TELMESSOS.CORE.Entities;
+
+namespace TELMESSOS.ENTITIES.Entity
+{
+    public class User:CoreEntity
+    {
+        [StringLength(30), Required]
+        public string Username { get; set; }
+
+        [StringLength(30), Required]
+        public string Password { get; set; }
+
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        public string Lastname { get; set; }
+    }
+}
